093bfd0269e62cd830bc70774e7c1e1f59d8574c
M1sm4tch <91552196+M1sm4tch@users.noreply.github.com>
2024-02-25 16:22:13 +0530

    Revert "Client and Server side validation"
    This reverts commit bc04f001a00c3a5fa095479c61ce65b95fe1b092.

--- server.js
+++ server.js
@@ -9,40 +9,15 @@ const app = express();
 
 const PORT = process.env.PORT || 2000;
 
-
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-let validatedData = [];
 
 app.get('/',(req, res)=>{
     res.render('index');
 });
 
-app.post('/submit', (req, res) => {
-    
-    const { userName, userEmail, userAge, userPassword, gender } = req.body;
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(userEmail)) {
-        res.render('form', { error: 'Invalid email address' });
-        return;
-    }
-
-    const userData = {
-        userName,
-        userEmail,
-        userAge,
-        userPassword,
-        gender
-    };
-    validatedData.push(userData);
-
-    res.render('success', { userData });
-});
 
 
 app.listen(PORT, () => {
